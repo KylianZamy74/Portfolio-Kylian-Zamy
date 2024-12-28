8eb0435ac237951247191731ec2e6ffe86b110e0
Kylian zamy <kylian.zamy@protonmail.com>
2024-12-28 11:43:10 +0100

    Initialisation du projet

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+
+import Link from "next/link"
+
+export default function about() {
+
+    return(
+        <>
+        <h1>Bienvenue sur la page about</h1>
+        <Link href={"/"}>Retourner à la page d&apos;accueil</Link>
+        </>
+    )
+}
