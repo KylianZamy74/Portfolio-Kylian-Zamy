552a879a0a3b0b0fe4c823d6d56ab0b80e4fb196
Kylian zamy <kzamy.74940@gmail.com>
2025-01-27 11:06:49 +0100

    feat: finitions des dictionnaires anglais/francais traduction de l'application terminé

--- my-project/src/components/Footer.tsx
+++ my-project/src/components/Footer.tsx
@@ -1,22 +1,25 @@
 import React from "react";
 import Link from "next/link";
+import { useTranslation } from "react-i18next";
 
 export default function Footer() {
+
+    const {t} = useTranslation();
 
     return (
         <footer className="bg-[#1E1E1E] text-[#FDFAD5] py-12 px-4">
         <div className="lg:mx-auto">
             {/* Informations de contact */}
             <div className="flex flex-col sm:flex-row sm:justify-start items-start mb-8">
-                <span className="text-lg mb-2 sm:mb-0 sm:mr-8">Email: <a href="mailto:Kylian.74940@gmail.com" className="text-[#F97316] hover:text-[#FDFAD5]">Kylian.74940@gmail.com</a></span>
-                <span className="text-lg">Phone: <a href="tel:+33684722698" className="text-[#F97316] hover:text-[#FDFAD5]">+33 6 84 72 26 98</a></span>
+                <span className="text-lg mb-2 sm:mb-0 sm:mr-8">{t("footer.email")} : <a href="mailto:Kylian.74940@gmail.com" className="text-[#F97316] hover:text-[#FDFAD5]">Kylian.74940@gmail.com</a></span>
+                <span className="text-lg">{t("footer.phone")} : <a href="tel:+33684722698" className="text-[#F97316] hover:text-[#FDFAD5]">+33 6 84 72 26 98</a></span>
             </div>
 
             {/* Texte principal */}
             <div className="text-center mb-12">
-                <p className="text-3xl font-bold mb-8">Ready to work together?</p>
+                <p className="text-3xl font-bold mb-8">{t("footer.ready")}</p>
                 <div className="border-2 border-[#FDFAD5] py-2 px-4 rounded text-3xl hover:bg-[#FDFAD5] hover:text-[#1E1E1E] duration-200 inline-block">
-                    <Link href="/contact">Get in touch</Link>
+                    <Link href="/contact">{t("footer.contact")}</Link>
                 </div>
             </div>
 
@@ -37,8 +40,8 @@ export default function Footer() {
             <div className="text-center mt-16 text-sm text-gray-400">
                 <p>&copy; 2025 Kylian Zamy. All rights reserved.</p>
                 <div className="flex justify-center mt-4 space-x-8">
-                    <Link href="/mentions-legales" className="hover:text-[#F97316]">Mentions légales</Link>
-                    <Link href="/politique-confidentialite" className="hover:text-[#F97316]">Politique de confidentialité</Link>
+                    <Link href="/mentions-legales" className="hover:text-[#F97316]">{t("footer.legacy")}</Link>
+                    <Link href="/politique-confidentialite" className="hover:text-[#F97316]">{t("footer.policy")}</Link>
                     <Link href="/cookies" className="hover:text-[#F97316]">Cookies</Link>
                 </div>
             </div>
--- my-project/src/pages/contact.tsx
+++ my-project/src/pages/contact.tsx
@@ -8,6 +8,7 @@ import {motion} from "framer-motion";
 import MenuOpen from "@/components/Modal/OpenMenuOnScroll";
 import Inner from "@/components/Layout/Inner";
 import { useAnimationGsapService, splitTextIntoChar } from "@/services/animationServices/useGsapAnimationService";
+import { useTranslation } from "react-i18next";
 import { useContactStore } from "@/store/ContactStore/useContactService";
 
 
@@ -28,6 +29,8 @@ export default function Contact() {
            setMessage,
            sendMail,
         } = useContactStore();
+
+    const {t} = useTranslation();
     
    const handlesubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -43,34 +46,34 @@ export default function Contact() {
            
             <Inner>
                 <div className="flex-col flex justify-center h-screen p-4 intro">
-                    <h1 className="text-[#FDFAD5] text-6xl">{splitTextIntoChar("Let's begin a")} <strong className="text-[#F97316]">{splitTextIntoChar("project.")}</strong></h1>
+                    <h1 className="text-[#FDFAD5] text-6xl">{splitTextIntoChar(t("contact.title"))} <strong className="text-[#F97316]">{splitTextIntoChar(t("contact.title_1"))}</strong></h1>
                 </div>
                 
                 <div className="lg:flex md:flex w-full justify-between space-y-12 md:px-24 lg:px-24">
                     <section className="px-4 w-2/3">
                         <form onSubmit={handlesubmit} className="text-[#FDFAD5] font-bold">
                             <div>
-                                <label  className="text-2xl flex-col flex" htmlFor="name">Name</label>
-                                <input className="bg-transparent mt-4 p-2 w-full" type="text"name="name" placeholder="What's your name ?" required onChange={(e) => setName(e.target.value)} />
+                                <label  className="text-2xl flex-col flex" htmlFor="name">{t("contact.name")}</label>
+                                <input className="bg-transparent mt-4 p-2 w-full" type="text"name="name" placeholder={t("contact.name_placehold")} required onChange={(e) => setName(e.target.value)} />
                             </div>
                             <div className="mt-8">
-                                <label  className="text-2xl flex-col flex" htmlFor="email">Email adress</label>
-                                <input className="bg-transparent mt-4 p-2 w-full" type="email"name="email" placeholder="yourmail@example.com" required onChange={(e) => setEmail(e.target.value)}/>
+                                <label  className="text-2xl flex-col flex" htmlFor="email">{t("contact.email")}</label>
+                                <input className="bg-transparent mt-4 p-2 w-full" type="email"name="email" placeholder={t("contact.email_placehold")} required onChange={(e) => setEmail(e.target.value)}/>
                             </div>
                             <div className="mt-8">
-                                <label  className="text-2xl flex-col flex" htmlFor="subject">Subject</label>
-                                <input className="bg-transparent mt-4 p-2 w-full" type="text"name="subject" placeholder="What's your project about ?" required onChange={(e) => setSubject(e.target.value)}/>
+                                <label  className="text-2xl flex-col flex" htmlFor="subject">{t("contact.subject")}</label>
+                                <input className="bg-transparent mt-4 p-2 w-full" type="text"name="subject" placeholder={t("contact.subject_placehold")} required onChange={(e) => setSubject(e.target.value)}/>
                             </div>
                             <div className="mt-8">
-                                <label className="text-2xl flex-col flex" htmlFor="number">Phone number</label>
-                                <input  className="bg-transparent mt-4 p-2 w-full" type="tel"name="number" placeholder="If you prefer a call." onChange={(e) => setPhone(e.target.value)}/>
+                                <label className="text-2xl flex-col flex" htmlFor="number">{t("contact.phone")}</label>
+                                <input  className="bg-transparent mt-4 p-2 w-full" type="tel"name="number" placeholder={t("contact.phone_placehold")} onChange={(e) => setPhone(e.target.value)}/>
                             </div>
                             <div className="mt-8">
-                                <label className="text-2xl flex-col flex" htmlFor="message">Message</label>
-                                <textarea className="bg-transparent w-full mt-4 p-2" name="message" placeholder="Hello Kylian, let's talk about how we can work together." required onChange={(e) => setMessage(e.target.value)}/>
+                                <label className="text-2xl flex-col flex" htmlFor="message">{t("contact.message")}</label>
+                                <textarea className="bg-transparent w-full mt-4 p-2" name="message" placeholder={t("contact.message_placehold")} required onChange={(e) => setMessage(e.target.value)}/>
                             </div>
                             <div className="mt-8 bg-[#FDFAD5] bg-fadedYellow text-center rounded p-2  w-full md:w-1/3 mx-auto">
-                                <button type="submit">Let&apos;s connect !</button>
+                                <button type="submit">{t("contact.connect")}</button>
                             </div>
                         </form>
                     </section>
@@ -83,7 +86,7 @@ export default function Contact() {
                             </div>
                         </div>
                         <div className="mt-8 text-lg">
-                            <h3 className="text-labelYellow mb-2">Useful links</h3>
+                            <h3 className="text-labelYellow mb-2">{t("contact.links")}</h3>
                             <div className="flex-col flex">
                                 <button className="text-left"><a className="text-left" href="https://www.linkedin.com/in/kylian-zamy-b0a5ab303">LinkedIn</a></button>
                                 <button className="text-left"><a  href="https://github.com/KylianZamy">Github</a></button>
