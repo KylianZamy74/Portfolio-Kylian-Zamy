6a4657de8066f73a0b46d323d50e5024e9f8657b
Kylian zamy <kzamy.74940@gmail.com>
2025-02-06 15:55:35 +0100

    refactor: ajout de la sectio faq tarduction avec i18n dans la page services

--- my-project/src/pages/services.tsx
+++ my-project/src/pages/services.tsx
@@ -1,6 +1,11 @@
 
 import Inner from '@/components/Layout/Inner';
-
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+  } from "@/components/ui/accordion"
 import { useModalStore } from "@/store/ModalStore/useModalStore";
 import Menu from "@/components/Modal/MenuModal";
 import Image from "next/image";
@@ -39,7 +44,7 @@ export default function Services() {
                     </div>
                 </section>
                 <section className="mt-12 space-y-12 mx-4">
-            <h2 className='text-5xl mt-8 mb-12 font-semibold'>{splitTextIntoWords(t("services-section.services.main-title"))}<strong className='text-beige'>{splitTextIntoWords(t("services-section.services.main-title-strong"))}</strong>  </h2>
+            <h2 className='text-5xl mt-8 mb-12 font-semibold text-center'>{splitTextIntoWords(t("services-section.services.main-title"))}<strong className='text-beige'>{splitTextIntoWords(t("services-section.services.main-title-strong"))}</strong>  </h2>
                     <article className='border-t-2 pt-8 border-b-2 pb-12'>
                         <h3 className='text-beige text-3xl font-bold'>{splitTextIntoWords(t("services-section.services.card_1.title"))}</h3>
                         <h4 className='text-2xl font-semibold my-4 text-anthra'>{splitTextIntoWords(t("services-section.services.card_1.subtitle"))}</h4>
@@ -81,7 +86,74 @@ export default function Services() {
                     </article>
                 </section>
 
-                <section className="min-h-screen flex justify-center items-center flex-col px-12 py-12">
+                <section className=" flex justify-center items-center flex-col mx-4 py-12">
+                    <div>
+                        <Image src="/Images/faq.svg" alt="Image faq" width={350} height={350}/>
+                    </div>
+                    <h2 className='text-5xl my-4 font-semibold'>FAQ</h2>
+                    <Accordion type="single" collapsible className="w-full text-anthra">
+                <AccordionItem value="item-1">
+                    <AccordionTrigger>{t("services-section.faq.faq-1.title")}</AccordionTrigger>
+                    <AccordionContent>
+                    {t("services-section.faq.faq-1.paragraph")}
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-2">
+                    <AccordionTrigger>{t("services-section.faq.faq-2.title")}</AccordionTrigger>
+                    <AccordionContent>
+                    {t("services-section.faq.faq-2.paragraph")}
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-3">
+                    <AccordionTrigger>{t("services-section.faq.faq-3.title")}</AccordionTrigger>
+                    <AccordionContent>
+                    {t("services-section.faq.faq-3.paragraph")}
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-4">
+                    <AccordionTrigger>{t("services-section.faq.faq-4.title")}</AccordionTrigger>
+                    <AccordionContent>
+                    {t("services-section.faq.faq-4.paragraph")}
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-5">
+                    <AccordionTrigger>{t("services-section.faq.faq-5.title")}</AccordionTrigger>
+                    <AccordionContent>
+                    {t("services-section.faq.faq-5.paragraph")}
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-6">
+                    <AccordionTrigger>{t("services-section.faq.faq-6.title")}</AccordionTrigger>
+                    <AccordionContent>
+                    {t("services-section.faq.faq-6.paragraph")}
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-7">
+                    <AccordionTrigger>{t("services-section.faq.faq-7.title")}</AccordionTrigger>
+                    <AccordionContent>
+                    {t("services-section.faq.faq-7.paragraph")}
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-8">
+                    <AccordionTrigger>{t("services-section.faq.faq-8.title")}</AccordionTrigger>
+                    <AccordionContent>
+                    {t("services-section.faq.faq-8.paragraph")}
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-9">
+                    <AccordionTrigger>{t("services-section.faq.faq-9.title")}</AccordionTrigger>
+                    <AccordionContent>
+                    {t("services-section.faq.faq-9.paragraph")}
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-10">
+                    <AccordionTrigger>{t("services-section.faq.faq-10.title")}</AccordionTrigger>
+                    <AccordionContent>
+                    {t("services-section.faq.faq-10.paragraph")}
+                    </AccordionContent>
+                </AccordionItem>
+                
+                </Accordion>
 
                 </section>
 
