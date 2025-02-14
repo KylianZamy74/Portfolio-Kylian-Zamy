2daf1c4d05b606bf1d6e17deb95c42b39adc7b9e
Kylian zamy <kzamy.74940@gmail.com>
2025-02-14 16:42:25 +0100

    ça continue encore et encore

--- my-project/next.config.ts
+++ my-project/next.config.ts
@@ -4,6 +4,27 @@ import i18n from './next-i18next.config';
 const nextConfig: NextConfig = {
   i18n,
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/api/:path*', // Applique les headers à toutes les routes API
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://kylian-zamy.dev', // Remplace par ton domaine ou utilise '*' si tu veux autoriser tous les domaines
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
